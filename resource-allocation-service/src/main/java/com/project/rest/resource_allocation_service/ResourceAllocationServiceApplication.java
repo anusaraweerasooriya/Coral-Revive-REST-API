687c1133235b94271c66ee93d78a5c7dc372dce6
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2024-07-07 18:56:37 +0530

    Initialized resource-allocation-service

---
+++ resource-allocation-service/src/main/java/com/project/rest/resource_allocation_service/ResourceAllocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.rest.resource_allocation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAllocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceAllocationServiceApplication.class, args);
+	}
+
+}
