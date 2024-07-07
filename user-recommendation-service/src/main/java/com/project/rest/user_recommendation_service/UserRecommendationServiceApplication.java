8f516b1716107b1b623bdbe65dd02450e3bb8be3
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2024-07-07 19:26:05 +0530

    Initialized user-recommendation-service

---
+++ user-recommendation-service/src/main/java/com/project/rest/user_recommendation_service/UserRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.rest.user_recommendation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ user-recommendation-service/src/test/java/com/project/rest/user_recommendation_service/UserRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rest.user_recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
