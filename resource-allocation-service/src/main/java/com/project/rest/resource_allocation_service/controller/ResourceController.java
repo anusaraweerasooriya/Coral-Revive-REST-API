c34c0515361efbd1ff166df42dd8cfa37f235ad2
DilshiWanniarachchi <dilshimwanniarachchi@gmail.com>
2024-10-28 08:46:22 +0530

    Resource controller url change

--- resource-allocation-service/src/main/java/com/project/rest/resource_allocation_service/controller/ResourceController.java
+++ resource-allocation-service/src/main/java/com/project/rest/resource_allocation_service/controller/ResourceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/resources")
+@RequestMapping("/resource-allocation-service/resources")
 public class ResourceController {
 
     @Autowired
